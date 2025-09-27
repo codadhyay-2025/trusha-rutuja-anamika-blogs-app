7859f2fd7f926688da224019936803d9608523f0
rutuja.date <daterutuja5646@gmail.com>
2025-09-27 16:22:37 +0530

    Register are Complete

---
+++ src/Component/Register/Register.js
@@ -0,0 +1,30 @@
+import "./Register.css"
+function Register(){
+    return(
+     <div className="backgroundColor">
+            <div className="registerBox">
+                <div className="registerBlog">Blogs</div>
+                <div className="registerContent">Publish your passions,your way....</div>
+                <hr/>
+                <div className="register">Register</div>
+                <div className="registerFeild">Name</div>
+                <div>
+                    <input type="text"  placeholder="Firstname Lastname" className="registerInputFeild"/>
+                </div>
+                <div className="registerFeild">Email-ID</div>
+                <div>
+                    <input type="text" placeholder="test@gmail.com" className="registerInputFeild"/>
+                </div>
+                <div className="registerFeild">Password</div>
+                <div>
+                    <input type="Password" placeholder="Password" className="registerInputFeild"/>
+                </div>
+                <div>
+                    <button className="registerButton">Register</button>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export  default Register;
