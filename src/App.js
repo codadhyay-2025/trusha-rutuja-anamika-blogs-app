bd51482d51837609fd58d9d1889618fce9040968
Anamika2424 <kuteanamika7@gmail.com>
2025-09-30 16:11:27 +0530

    completed the blogs and createnewpost screens

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './Components/Dashboard/Dashboard';
+// import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Dashboard/Header/Header';
-import Register from './Component/Register/Register';
-
+// import Register from './Component/Register/Register';
+// import Blogs from './Components/Blogs/Blogs';
+import Blogs from './Components/Dashboard/Blogs/Blogs';
+import CreateNewPost from './Components/CreateNewPost/CreateNewPost';
 function App() {
   return (
     <div>
       <Header />
-      <Dashboard />
-      <Register/>
+      {/* <Dashboard /> */}
+      {/* <Register/> */}
+      <Blogs/>
+      {/* <CreateNewPost/> */}
     </div>
 
   );
---
+++ src/Components/CreateNewPost/CreateNewPost.js
@@ -0,0 +1,22 @@
+import "./CreateNewPost.css"
+function CreateNewPost(){
+    return (
+        <div className="mainSectionOfCreateNewPost">
+            <div className="cardOfCreateNewPost">
+                    <div>
+                        <input type="text" placeholder="Title" className="titleOfCreateNewPost"/>
+                    </div>
+                    <div>
+                        <textarea className="textareaField" placeholder="Description" rows={20}>Description</textarea>
+                    </div>
+                    <div className="btnSection">
+                        <div><button className="btnOfCreateNewPost">Cancel</button></div>
+                        <div><button className="btnOfCreateNewPost">Save</button></div>
+
+                    </div>
+            </div>
+        </div>
+    )
+
+}
+export default CreateNewPost;
